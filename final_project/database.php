9de39b73b067c51e4661c6f9ce323066e81f5b3c
Alex Stampfl <79325886+AlexStampfl@users.noreply.github.com>
2023-09-09 16:04:31 -0500

    Update database.php

--- final_project/database.php
+++ final_project/database.php
@@ -1,7 +1,9 @@
 <?php
 //***MAKE SURE YOU CHANGE YOUR DBNAME, USERNAME & PASSWORD HERE
-//CHECK phpmyadmin that you have '' database
+//CHECK '' that you have '' database
 //and that it is named like this
+
+// This information has been left blank intentionally to hide sensitive information.
     $dsn = '';
     $username = '';
     $password = '';
